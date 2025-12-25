71fb8c6a81f10b0dddbb0182fdbab2e4edb562bb
lazywalker <lazywalkerz@gmail.com>
2025-12-25 20:51:30 +0800

    feat(ecs): add ECS plugin for managing EDNS Client Subnet options and update example configuration

--- src/plugin/factory.rs
+++ src/plugin/factory.rs
@@ -481,6 +481,8 @@ pub fn initialize_all_exec_plugin_factories() {
         Lazy::force(&crate::plugins::flow::prefer_ipv4::PREFER_IPV4_PLUGIN_EXEC_FACTORY);
         Lazy::force(&crate::plugins::flow::prefer_ipv6::PREFER_IPV6_PLUGIN_EXEC_FACTORY);
 
+        Lazy::force(&crate::plugins::executable::ecs::ECS_PLUGIN_EXEC_FACTORY);
+
         // Initialize the exec factory system
         initialize_exec_plugin_factories();
 
